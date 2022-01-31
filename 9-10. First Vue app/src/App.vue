86d6bdf5ad039ee3817de2555d778d6e1ecce64f
Sento <vsornosarisen@gmail.com>
2022-01-31 20:37:37 +0100

    Initial code
    New vue app

---
+++ 9-10. First Vue app/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+    <section>
+        <h2> My friends </h2>
+        <ul>
+            <li> Friend 1 </li>
+            <li> Friend 2 </li>
+        </ul>
+    </section>
+</template>
+
+<script>
+    export default {
+        data() { 
+            return {
+                friends: [
+                    {
+                        id: 'manuel',
+                        name: 'Manuel Lorenz',
+                        phone: ' 0123 45678 90',
+                        email: 'manuel@localhost.com'
+                    },
+                    {
+                        id: 'julie',
+                        name: 'Julie Jones',
+                        phone: ' 0987 65432 10',
+                        email: 'julie@localhost.com'
+                    }
+                ]
+            }
+        }
+    }
+</script>
